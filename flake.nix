a9ced61f52c4ddb087b1b4fd9381e64bf1dbe7a6
replicapra <replicapra@outlook.com>
2025-07-03 21:44:33 +0200

    chore(flake): create flake outputs with snowfall-lib

--- flake.nix
+++ flake.nix
@@ -8,11 +8,11 @@
     snowfall-lib.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }: {
+  outputs =
+    inputs:
+    inputs.snowfall-lib.mkFlake {
+      inherit inputs;
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+      src = ./.;
+    };
 }
