06b912d6475c2c1c921941568f6af5497ed8d21b
replicapra <154707993+replicapra@users.noreply.github.com>
2026-03-01 15:05:27 +0100

    chore(inputs): add snowfall-lib

--- flake.nix
+++ flake.nix
@@ -2,10 +2,19 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs = {
+      url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    };
+
+    snowfall-lib = {
+      url = "github:snowfallorg/lib";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, ... }: {
 
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
